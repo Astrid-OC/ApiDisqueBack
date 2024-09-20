bf0e75d37ec01187197160f39156ae3f88931ee8
Astrid-OC <astridtournant@gmail.com>
2024-09-20 09:16:07 +0200

    User fait, JWT en cour page 48

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,17 +2,42 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use App\Entity\Disque;
 use App\Entity\Chansons;
 use App\Entity\Chanteur;
-use App\Entity\Disque;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         
+        //Création d'unn user normal
+        $user = new User();
+        $user->setEmail("user@disqueapi.com");
+        $user->setRoles(["ROLE_USER"]);
+
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, "password"));
+        $manager->persist($user);
+
+        //Création d'user admin
+        $userAdmin = new User();
+        $userAdmin->setEmail("admin@disqueapi.com");
+        $userAdmin->setRoles(["ROLE_ADMIN"]);
+        //cette méthode permet de retourner le mdp encodé de l'uti.
+        $userAdmin->setPassword($this->userPasswordHasher->hashPassword($userAdmin, "password"));
+        $manager->persist($userAdmin);
+
         //Création des chanteurs.
         $listChanteur = [];
         for ($i=0; $i < 10; $i++) 
